62f6a062991cf21a83aab33c90ee1ade53615c93
akshayg <akshay.guruprasad2023@gmail.com>
2023-11-04 06:36:00 +0530

    init

---
+++ ums-engine/src/main/java/io/indream/umsengine/UmsEngineApplication.java
@@ -0,0 +1,13 @@
+package io.indream.umsengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmsEngineApplication.class, args);
+	}
+
+}
---
+++ ums-engine/src/test/java/io/indream/umsengine/UmsEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.indream.umsengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
